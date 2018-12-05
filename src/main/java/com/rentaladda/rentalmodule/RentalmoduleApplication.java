4a9bba724fa05d5aecc0c8307a02a8fce29c06bc
Nandan1981 <Nandan1981>
2018-12-05 17:31:00 +0530

    Initial commit

---
+++ src/main/java/com/rentaladda/rentalmodule/RentalmoduleApplication.java
@@ -0,0 +1,12 @@
+package com.rentaladda.rentalmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalmoduleApplication.class, args);
+    }
+}
