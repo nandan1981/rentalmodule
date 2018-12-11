eb8c54593f8ee69113ce844b1907bccc9f4900fb
Nandan1981 <Nandan1981@gmail.com>
2018-12-11 12:44:24 +0530

    Some stupid UX changes

--- src/main/java/com/rentaladda/controller/RentalController.java
+++ src/main/java/com/rentaladda/controller/RentalController.java
@@ -1,4 +1,17 @@
 package com.rentaladda.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RentalController {
+
+    @RequestMapping(value = "/rentalPage")
+    public String showRentalPage(Model model){
+        model.addAttribute("isEnabledSignUp",true);
+        model.addAttribute("isEnabledLogin",true);
+        model.addAttribute("isLoggedIn",false);
+        return "rentalPage";
+    }
 }
